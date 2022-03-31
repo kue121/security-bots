a95dfc854f972303fff910d65d3a4201245b39be
kue121 <102326068+kue121@users.noreply.github.com>
2022-03-31 12:46:30 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -37,7 +37,7 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "s!";
+const prefix = "!";
 const cooldown = new Set();
 const cdtime = 5;
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,7 +66,7 @@ calli.on("message", message => {
   }
 });
 ///////////////////////////////////////////////////////////////////////////////
-calli.login("")
+calli.login("OTU4NDYxMzM2MTMwNTYwMDEw.YkNqrQ.2UDa7jsZ64o-blOb7ugYl6oHszs")
 ///////////////////////////////////////////////////////////////////////////////
 const callienabled  = "";
 const callidisabled = "";
